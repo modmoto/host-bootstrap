3491e2dc2ac5e461125f9c4f8b300414819b6fee
modmoto <simonheiss87@gmail.com>
2021-09-14 17:09:49 +0200

    asd

---
+++ DbBackups/RunDatbaseBackup.sh
@@ -0,0 +1,34 @@
+﻿#!/bin/bash
+
+pushd /usr/local/db-backups/
+today=$(date '+%Y-%m-%d_%H-%M-%S')
+
+mkdir $today
+echo "created $today in $(pwd)"
+
+pushd $today
+
+mongodump --uri="$1" -o ./identity-service
+mongodump --uri="$2" -o ./fading-flame-service
+
+popd
+
+directoryCount=0
+lastDir=""
+for currentDir in $(ls -1vdr */) ; do
+  ((directoryCount+=1))
+  lastDir=$currentDir
+done
+
+echo "There are $directoryCount directories"
+echo "oldest backup is $lastDir"
+
+if [ $directoryCount -gt 7 ]
+then
+  echo "have more than 7 backups, deleting backup: $lastDir"
+  rm -rf "$lastDir"
+fi
+
+popd
+
+
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-docker network create caddy
-cd ./caddy
-docker-compose up -d
+## Getting up the services 
+- docker network create caddy
+- cd ./caddy
+- docker-compose up -d
 
-cd ../portainer/
-docker-compose up -d
+- cd ../portainer/
+- docker-compose up -d
+
+## Setting up backup scripts
+- install mongo to have the tools (container sucked so far)
+- run a cron job with:
