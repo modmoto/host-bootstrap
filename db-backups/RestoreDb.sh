e0d663fdab97ca291ec7e37f7cc800760267282c
modmoto <simonheiss87@gmail.com>
2021-09-23 21:16:55 +0200

    nv

--- db-backups/RestoreDb.sh
+++ db-backups/RestoreDb.sh
@@ -1,3 +1,7 @@
-﻿mongodump --uri="connectionstringFadingFlameProd" /o ./fading-flame
+﻿#locally
+mongodump --uri="connectionstringFadingFlameProd" /o ./fading-flame
+mongorestore --uri="connectionstringFadingFlameTEST" fading-flame/
 
-mongorestore --uri="connectionstringFadingFlameProd" fading-flame/
+#ssh into service when big data
+mongodump --uri="connectionstringFadingFlameProd"
+mongorestore --uri="mongodb://user:password@localhost:27017" dump/
